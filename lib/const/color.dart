26e2cffdbe90bbddd5b40a208a2339f97d3ee7bf
윾승도 <66948129+tmdeh@users.noreply.github.com>
2023-11-13 23:18:39 +0900

    Delete color.dart

--- lib/const/color.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color PRIMARY_COLOR = Colors.black;
-final Color SECOND_COLOR = Colors.black.withOpacity(0.6);
-const Color THIRD_COLOR = Colors.white;
