5f78bc75738e01cbac28c375a83a1aef22e2d544
윾승도 <66948129+tmdeh@users.noreply.github.com>
2024-01-03 02:42:56 +0900

    모델 객체 생성

--- lib/domain/model/record.dart
+++ lib/domain/model/record.dart
@@ -1,18 +1,20 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:pocket_money_management_app/data/model/category.dart';
-import 'package:pocket_money_management_app/data/model/payment_type.dart';
+import 'package:pocket_money_management_app/domain/model/payment_type.dart';
+import 'package:pocket_money_management_app/domain/model/category.dart';
 
 part 'record.freezed.dart';
 part 'record.g.dart';
 
+
 @freezed
 class Record with _$Record {
+
   factory Record({
     int? id,
     required int timestamp,
     required Category category,
     @JsonKey(name: 'payment_type')
-    required PaymentType paymentType,
+    required PaymentType paymentType
   }) = _Record;
 
   factory Record.fromJson(Map<String, dynamic> json) => _$RecordFromJson(json);
